09e5ed98ab783310d3909788d0f3aee9e9aca100
hanene <ghribihanen17@gmail.com>
2021-05-05 12:59:06 +0100

    evanescence

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import "./styles.css";
 import imageInSrc from './imageInSrc.jpg';
 
 
+
 function App() {
-  let YourName = "Amylee";
+  let YourName = "Amylee ❤️❤️";
   return (
  
     <div style= {{ border: "solid 1px black", maxWidth: "100vw"}}> 
@@ -18,11 +19,12 @@ function App() {
 
  <img src= './imageInPublic.jpg' className="App-pub"  alt="imageInPublic"></img>
  <br/>
- 
-<video width="320" height="240" controls>
-<iframe width="320" height="240" src="https://www.youtube.com/embed/QIvxCf10fe4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen type="video/mp4"></iframe>
+ <video width="320" height="240" controls>
+
+<source src="./video/evan.mp4" type="video/mp4" />
 </video>
 
+ 
   </div>
 
 
